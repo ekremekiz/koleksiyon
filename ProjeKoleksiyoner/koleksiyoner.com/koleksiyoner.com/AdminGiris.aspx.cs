dbe5961b598c5830aced844957739c0a6e44f821
Ekrem Ekiz <ekremekz@gmail.com>
2017-11-30 13:30:39 +0300

    AdminPanel unutulan detaylar eklendi.

--- ProjeKoleksiyoner/koleksiyoner.com/koleksiyoner.com/AdminGiris.aspx.cs
+++ ProjeKoleksiyoner/koleksiyoner.com/koleksiyoner.com/AdminGiris.aspx.cs
@@ -4,14 +4,32 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace koleksiyoner.com
 {
     public partial class AdminGiris : System.Web.UI.Page
     {
+        SQLBaglanti baglan = new SQLBaglanti();
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlCommand komut = new SqlCommand("Select * from Admin where AdminAdi='" + TextBox1.Text + "' and AdminSifre='" + TextBox2.Text + "' ", baglan.baglan());
+            SqlDataReader dr = komut.ExecuteReader();
+            if (dr.Read())
+            {
+                Response.Redirect("UrunEkle.aspx");
+            }
+            else
+            {
+                Label1.Text = "Kullanıcı Adı veya Şifre Hatalı";
+            }
         }
     }
 }
