57d7dbe79f8dc23803fa03c5d47ca79a1ed8fc64
Ekrem Ekiz <ekremekz@gmail.com>
2017-11-30 13:24:01 +0300

    AdminPanel tamamiyle eklendi.

---
+++ ProjeKoleksiyoner/koleksiyoner.com/koleksiyoner.com/SQLBaglanti.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace koleksiyoner.com
+{
+    public class SQLBaglanti
+    {
+        public SqlConnection baglan()
+        {
+            SqlConnection baglanti = new SqlConnection("Data Source=.; Initial Catalog=koleksiyoner; Integrated Security=True ");
+            baglanti.Open();
+            SqlConnection.ClearPool(baglanti);
+            SqlConnection.ClearAllPools();
+
+            return (baglanti);
+        }
+    }
+}
---
+++ ProjeKoleksiyoner/koleksiyoner.com/koleksiyoner.com/UrunEkle.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace koleksiyoner.com
+{
+    public partial class UrunEkle : System.Web.UI.Page
+    {
+        SQLBaglanti baglan = new SQLBaglanti();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //kategori çek
+            SqlCommand cmd = new SqlCommand("Select * from Kategori", baglan.baglan());
+            SqlDataReader dr = cmd.ExecuteReader();
+            DropDownList1.DataTextField = "KategoriAdi";
+            DropDownList1.DataValueField = "KategoriID";
+            DropDownList1.DataSource = dr;
+            DropDownList1.DataBind();
+        }
+
+        /*protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("insert into Urun (UrunResimYolu,UrunAdi,UrunFiyat,Aciklama,EklenmeTarihi,IhaleSuresi,KategoriID) values ('" + FileUpload1.FileName + "','" + TextBox1.Text + "','" + TextBox2.Text + "','" + TextBox4.Text + "','" + DateTime.Now.ToString("dd.MM.yyyy") + "','" + TextBox3.Text +"','" + DropDownList1.SelectedValue +"')", baglan.baglan());
+            cmd.ExecuteNonQuery();
+            
+        }*/
+
+        protected void Button1_Click1(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("insert into Urun (UrunResimYolu,UrunAdi,UrunFiyat,Aciklama,EklenmeTarihi,IhaleSuresi,KategoriID) values ('" + FileUpload1.FileName + "','" + TextBox1.Text + "','" + TextBox2.Text + "','" + TextBox4.Text + "','" + DateTime.Now.ToString("MM.dd.yyyy ") + "','" + TextBox3.Text + "','" + DropDownList1.SelectedValue + "')", baglan.baglan());
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
